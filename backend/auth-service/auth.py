03537f7897de1432c08fcffbf6e67c5291642fce
Chong Chin Herng <chongahzong@gmail.com>
2024-08-08 12:30:47 +0800

    Remove unused method

--- backend/auth-service/auth.py
+++ backend/auth-service/auth.py
@@ -26,35 +26,32 @@ def login():
     # To do
     return 'Login'
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            email = data.get('email')
-            password = data.get('password')
-            confirm_password = data.get('confirm_password')
-            
-            # Check if email already exists
-            existing_user = db.users.find_one({'email': email})
-            if existing_user:
-                return jsonify({'message': 'Email already exists', 'status': 'fail'}), 400
-            
-            # Insert new user
-            db.users.insert_one({
-                'email': email,
-                'password': password
-            })
-            
-            response_data = {
-                'message': "Account registered successfully",
-                'status': 'success',
-            }
-            return jsonify(response_data), 200
-        else:
-            return jsonify({'error': 'Unsupported Media Type, content type must be application/json'}), 415
+    if request.is_json:
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        
+        # Check if email already exists
+        existing_user = db.users.find_one({'email': email})
+        if existing_user:
+            return jsonify({'message': 'Email already exists', 'status': 'fail'}), 400
+        
+        # Insert new user
+        db.users.insert_one({
+            'email': email,
+            'password': password
+        })
+        
+        response_data = {
+            'message': "Account registered successfully",
+            'status': 'success',
+        }
+        return jsonify(response_data), 200
     else:
-        return jsonify({'error': 'Method Not Allowed'}), 405
+        return jsonify({'error': 'Unsupported Media Type, content type must be application/json'}), 415
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
